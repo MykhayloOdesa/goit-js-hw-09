1b8d681fd5c2996285bd45595322ce16ea02c6ae
MykhayloOdesa <110163150+MykhayloOdesa@users.noreply.github.com>
2023-01-01 18:40:33 +0000

    Deploying to gh-pages from @ MykhayloOdesa/goit-js-hw-09@22e3a7fcfc03b4927eb7b1e7a5367757ec33891d 🚀

---
+++ 01-color-switcher.4d637b53.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(t){r=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.currentTarget.disabled=!0,e.removeAttribute("disabled")})),e.addEventListener("click",(function(e){clearInterval(r),e.currentTarget.disabled=!0,t.removeAttribute("disabled")})),e.disabled=!0;let r=null;
+//# sourceMappingURL=01-color-switcher.4d637b53.js.map
