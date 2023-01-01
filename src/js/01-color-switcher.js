22e3a7fcfc03b4927eb7b1e7a5367757ec33891d
MykhayloOdesa <m.yervachov@gmail.com>
2023-01-01 20:40:00 +0200

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -21,3 +21,31 @@
 // function getRandomHexColor() {
 //   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 // }
+
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+
+startButton.addEventListener('click', onStartButtonClick);
+stopButton.addEventListener('click', onStopButtonClick);
+stopButton.disabled = true;
+let timerId = null;
+
+function onStartButtonClick(event) {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  event.currentTarget.disabled = true;
+  stopButton.removeAttribute('disabled');
+}
+
+function onStopButtonClick(event) {
+  clearInterval(timerId);
+
+  event.currentTarget.disabled = true;
+  startButton.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
