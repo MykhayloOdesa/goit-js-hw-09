e2730dc8dddd5000d8c4e1639b2b464d82283e28
MykhayloOdesa <m.yervachov@gmail.com>
2023-01-02 00:43:30 +0200

    task 2 done

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -69,12 +69,14 @@
 // Якщо користувач вибрав валідну дату (в майбутньому), кнопка «Start» стає активною.
 // Кнопка «Start» повинна бути неактивною доти, доки користувач не вибрав дату в майбутньому.
 // Натисканням на кнопку «Start» починається відлік часу до обраної дати з моменту натискання.
+
 // Відлік часу
 // Натисканням на кнопку «Start» скрипт повинен обчислювати раз на секунду, скільки часу залишилось до вказаної дати,
 // і оновлювати інтерфейс таймера, показуючи чотири цифри: дні, години, хвилини і секунди у форматі xx: xx: xx: xx.
 
 // Кількість днів може складатися з більше, ніж двох цифр.
 // Таймер повинен зупинятися, коли дійшов до кінцевої дати, тобто 00:00:00:00.
+
 // НЕ БУДЕМО УСКЛАДНЮВАТИ
 // Якщо таймер запущений, для того щоб вибрати нову дату і перезапустити його - необхідно перезавантажити сторінку.
 
@@ -118,3 +120,85 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
+
+const startButton = document.querySelector('button[data-start]');
+const timerContainer = document.querySelector('.timer');
+
+startButton.addEventListener('click', onStartButtonClick);
+startButton.disabled = true;
+let chosenTime = null;
+let deltaTime = null;
+let timerId = null;
+
+const flatpickRef = flatpickr('#datetime-picker', {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    setTimeout(() => {
+      if (selectedDates[0].getTime() < Date.now()) {
+        Notiflix.Notify.failure('Please choose a date in the future');
+        return;
+      }
+      startButton.removeAttribute('disabled');
+      chosenTime = selectedDates[0];
+    }, 0);
+  },
+});
+
+function onStartButtonClick(event) {
+  event.currentTarget.disabled = true;
+
+  timerId = setInterval(() => {
+    deltaTime = chosenTime.getTime() - Date.now();
+
+    const { days, hours, minutes, seconds } = convertMs(deltaTime);
+
+    if (deltaTime <= 0) {
+      clearInterval(timerId);
+      flatpickRef.element.disabled = false;
+    } else {
+      updateTimer({ days, hours, minutes, seconds });
+    }
+  }, 1000);
+}
+
+function updateTimer({ days, hours, minutes, seconds }) {
+  timerContainer.firstElementChild.firstElementChild.textContent =
+    addLeadingZero(`${days}`);
+  timerContainer.children[1].firstElementChild.textContent = addLeadingZero(
+    `${hours}`
+  );
+  timerContainer.children[2].firstElementChild.textContent = addLeadingZero(
+    `${minutes}`
+  );
+  timerContainer.lastElementChild.firstElementChild.textContent =
+    addLeadingZero(`${seconds}`);
+
+  flatpickRef.element.disabled = true;
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
